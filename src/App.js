3059f914ca7a3d714241f4930764d6928d9e4c09
Sunnn-mac <94742659+sunnn-mac@users.noreply.github.com>
2021-12-28 23:36:34 +0900

    完了

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      // エラー時
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      // .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      // エラー時
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
     </div>
   );
 }
